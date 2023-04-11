d57195e87e02810c67e64e569ace12d5bbe8f081
georgiybaryshnikov <85167278+georgiybaryshnikov@users.noreply.github.com>
2023-04-11 13:14:15 +0300

    ProgramEnd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк, формирует массив из строк,
+// длина которых меньше либа равна 3 символа. Первоначальный массив можно ввести с клавиатуры
+// или задать на старте выполнения алгоритма. При решении не рекомнуется пользоваться
+// коллекциями, лучше обойтись исключительно массивами.
+
+
+//Решение
+
+string[] array1 = new string[6] { "hello", "computer science", "67p", "wrd", "fw", "work" };
+string[] array2 = new string[array1.Length];
+void ArraySecond(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArraySecond(array1, array2);
+PrintArray(array2);
